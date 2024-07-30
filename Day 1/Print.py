b9a23d9234aba2100269ff84bee775f93fed3104
alwazirf <alwazirf@gmail.com>
2024-07-30 22:35:08 +0700

    feat: day 1 code in python

---
+++ Day 1/Print.py
@@ -0,0 +1,24 @@
+print("Hello Alwa")
+
+#print modifier
+print("Hello bro my name is \"Alwa\" ")
+
+#Print new  line
+print("Hello World \nHello World!!")
+
+#contatenation
+print("Hello" + "Alwa")
+
+
+#######################
+print("Hello " + input("What is your name?") + "!")
+
+
+print("---------------------------------")
+# this is section for variable
+name = "Alwa"
+print(name)
+
+name = len(input("What is your name?"))
+print(name)
+
