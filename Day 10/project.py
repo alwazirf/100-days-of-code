66d2462b36485f1350d5c4eddf7d0423fff2b430
alwazirf <alwazirf@gmail.com>
2024-08-04 20:50:21 +0700

    refactor: day 10 project calculator added logo and refactor structure code

--- Day 10/project.py
+++ Day 10/project.py
@@ -1,3 +1,4 @@
+from art import logo
 def add(n1,n2):
     return n1+n2
 
@@ -17,25 +18,26 @@ operations = {
     "/" : divide
 }
 
-first_num = int(input("What's the first number? "))
-for symbol in operations:
-    print(symbol)
-operand  = input("Pick an operation: ")
-second_num = int(input("What's the second number? "))
-total = 0
-continue_calc = True
-
+def calculator():
+    print(logo)
     
-while continue_calc:
-    calculation_function = operations[operand]
-    total = calculation_function(first_num, second_num)
+    first_num = float(input("What's the first number? "))
+    for symbol in operations:
+        print(symbol)
+    continue_calc = True
 
-    print(f"{first_num} {operand} {second_num} = {total}")
-    
-    ask_continue = input(f"Do you want to continue calculating with {total}? Type 'y' or 'n': ")
-    if ask_continue == "n":
-        continue_calc = False
-    else:
-        first_num = total
-        operand = input("Pick an operation: ")
-        second_num = int(input("What's the second number? "))
+        
+    while continue_calc:
+        operand  = input("Pick an operation: ")
+        second_num = float(input("What's the second number? "))
+        calculation_function = operations[operand]
+        total = calculation_function(first_num, second_num)
+
+        print(f"{first_num} {operand} {second_num} = {total}")
+        
+        ask_continue = input(f"Do you want to continue calculating with {total}? Type 'y' or 'n': ")
+        if ask_continue == "n":
+            continue_calc = False
+        else:
+            first_num = total
+calculator()
