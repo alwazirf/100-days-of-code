d38e001fd2f1712e10ac89c3dbecbfd7f29beb36
alwazirf <alwazirf@gmail.com>
2024-08-26 23:56:57 +0700

    refactor: Day 27 project miles to kilo refactor calculate

--- Day 27/project.py
+++ Day 27/project.py
@@ -7,9 +7,9 @@ window.config(padx=20, pady=20)
 output = 0
 KM = 1.609344
 
-def converter():
+def miles_to_km():
     miles = entry.get()
-    new_output = int(miles) * KM
+    new_output = round(float(miles) * KM)
     label3.config(text=new_output)
 
 entry = Entry(width=20)
@@ -24,7 +24,7 @@ label3.grid(column=1, row=1)
 label4 = Label(text="Km")
 label4.grid(column=2, row=1)
 
-button = Button(text="Calculate", command=converter)
+button = Button(text="Calculate", command=miles_to_km)
 button.grid(column=1, row=2)
 
 window.mainloop()
