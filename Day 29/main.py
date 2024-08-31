87ca07475381fd07db17a314633300086399b269
alwazirf <alwazirf@gmail.com>
2024-08-31 22:57:25 +0700

    refactor: optimal function generate_password

--- Day 29/main.py
+++ Day 29/main.py
@@ -1,27 +1,22 @@
 from tkinter import *
 from tkinter import messagebox
 FONT_NAME = "Courier"
-import random
+from random import randint, choice, shuffle
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def password_generator():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    nr_letter = random.randint(8,10)
-    nr_symbols = random.randint(2,4)
-    nr_numbers = random.randint(2,4)
-
     password = (
-        [random.choice(letters) for _ in range(nr_letter)] +
-        [random.choice(symbols) for _ in range(nr_symbols)] +
-        [random.choice(numbers) for _ in range(nr_numbers)]
+        [choice(letters) for _ in range(randint(8,10))] +
+        [choice(symbols) for _ in range(randint(2,4))] +
+        [choice(numbers) for _ in range(randint(2,4))]
     )
-    random.shuffle(password)
+    shuffle(password)
 
-    newPassword = ""
-    for i in password:
-        newPassword += i
+    newPassword = "".join(password)
     password_entry.insert(0, newPassword)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
