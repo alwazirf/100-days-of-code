ca70ae4407be9a65a50b33aa73379320ff143144
alwazirf <alwazirf@gmail.com>
2024-09-06 00:17:32 +0700

    feat, refactor: Day 34 added feature to check right or wrong with color background and add score automatic, refactor code ui to improve user experience

--- Day 34/data.py
+++ Day 34/data.py
@@ -1,7 +1,12 @@
+import requests
 question_data = []
 
-import requests
+parameters = {
+    "amount": 18,
+    "type": "boolean",
+    "category": 18
+}
 
-req = requests.get("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=boolean")
+req = requests.get("https://opentdb.com/api.php", params=parameters)
 req.raise_for_status()
 question_data = req.json()["results"]
--- Day 34/ui.py
+++ Day 34/ui.py
@@ -25,10 +25,10 @@ class QuizInterface:
         
         right_img = PhotoImage(file="./images/true.png")
         wrong_img = PhotoImage(file="./images/false.png")
-        self.right_button = Button(image=right_img, highlightthickness=0)
+        self.right_button = Button(image=right_img, highlightthickness=0, command=self.right_answer)
         self.right_button.grid(row=2, column=0)
         
-        self.wrong_button = Button(image=wrong_img, highlightthickness=0)
+        self.wrong_button = Button(image=wrong_img, highlightthickness=0, command=self.wrong_answer)
         self.wrong_button.grid(row=2, column=1)
         
         self.get_next_question()
@@ -36,5 +36,27 @@ class QuizInterface:
         self.window.mainloop()
         
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
+            self.right_button.config(state="disabled")
+            self.wrong_button.config(state="disabled")
+        
+    def right_answer(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+        
+    def wrong_answer(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+        
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+            
+        self.window.after(1000, self.get_next_question)
