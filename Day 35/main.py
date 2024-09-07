9fd926d382436b42f15f55a3f17bf1063e488fd1
alwazirf <alwazirf@gmail.com>
2024-09-08 00:58:06 +0700

    feat: Day 35 using api authenticate token

---
+++ Day 35/main.py
@@ -0,0 +1,25 @@
+import requests
+api_key = "e4bee0b9a9481421f6355b0e02e3044d"
+city = "London"
+latitude = 18.643061
+longtitude = -91.830490
+URI = "https://api.openweathermap.org/data/2.5/weather"
+
+parameters = {
+    "lat": latitude,
+    "lon": longtitude,
+    "appid": api_key,
+    "cnt": 4
+}
+
+req = requests.get(URI, params=parameters)
+req.raise_for_status()
+all_data = req.json()
+weather_id = all_data["weather"][0]["id"]
+will_rain = False
+print(all_data["weather"][0])
+if weather_id < 700:
+    will_rain = True
+    
+if will_rain:
+    print("Bring an umrella")
